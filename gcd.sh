239ebe03eaf30e26b766b329b5a7fe66c75a5c3e
Feng Yiwei <a2235yf@aiit.ac.jp>
2022-08-06 00:25:20 +0900

    edited

--- gcd.sh
+++ gcd.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-#数字のチェック
-expr $1 + 1 >&/dev/null
-ret1=$?
 
-expr $2 + 1 >&/dev/null
-ret2=$?
-
-if [ "$ret1" -ge 2 ] || [ "$ret2" -ge 2 ];then
-  echo "Input is not a number"
-  exit 1
-fi
 
 while [ 0 -lt "$1" ]
 do
