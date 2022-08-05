c2823b8f20021b9163df0c8357d9faedb7507372
Feng Yiwei <a2235yf@aiit.ac.jp>
2022-08-06 00:46:43 +0900

    edited

--- gcd.sh
+++ gcd.sh
@@ -2,13 +2,14 @@
 
 #数字のチェック
 if [[ "$1" =~ ^[0-9]+$ ]] || [[ "$2" =~ ^[0-9]+$ ]];then
+  while [ 0 -lt "$1" ]
+  do
+    t=`expr $2 % $1`
+    set $t $1
+  done
+  echo $2
+else
   echo "Input is not a number"
   exit 1
 fi
 
-while [ 0 -lt "$1" ]
-do
-  t=`expr $2 % $1`
-  set $t $1
-done
-echo $2
